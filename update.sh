c5f49a585f38337532f28785bdf04da83d11544f
npit <pittarasnikif@gmail.com>
2017-10-17 23:50:27 +0300

    Update dotfiles

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cp -v ~/.tmux.conf ~/.vimrc ./dotfiles/
-git add -u && git commit -m "Updated dotfiles"
-git push origin master
+git add -u && git commit -m "Update dotfiles"
+git push -f origin master
